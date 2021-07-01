7651f4c0b9e06490f6cb55fdb1cb2b533d0d441d
ElektroKill <skullmaster0103@gmail.com>
2021-07-01 21:26:54 +0200

    fix version

--- ILSpy/Properties/AssemblyInfo.template.cs
+++ ILSpy/Properties/AssemblyInfo.template.cs
@@ -9,7 +9,7 @@ using System.Diagnostics.CodeAnalysis;
 
 #endregion
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ILSpy")]
@@ -38,11 +38,11 @@ using System.Diagnostics.CodeAnalysis;
 
 internal static class RevisionClass
 {
-	public const string Major = "3";
-	public const string Minor = "2";
+	public const string Major = "4";
+	public const string Minor = "0";
 	public const string Build = "0";
 	public const string Revision = "$INSERTREVISION$";
 	public const string VersionName = "beta1";
-	
+
 	public const string FullVersion = Major + "." + Minor + "." + Build + ".$INSERTREVISION$$INSERTBRANCHPOSTFIX$$INSERTVERSIONNAMEPOSTFIX$";
 }
