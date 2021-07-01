55db1ddb2b0c9f699579b14f3b4c6d55ea81f0f1
ElektroKill <skullmaster0103@gmail.com>
2021-07-01 19:36:35 +0200

    Fixed printing of ModifiedType

--- ICSharpCode.Decompiler/TypeSystem/ModifiedType.cs
+++ ICSharpCode.Decompiler/TypeSystem/ModifiedType.cs
@@ -1,14 +1,14 @@
 ﻿// Copyright (c) 2018 AlphaSierraPapa for the SharpDevelop Team
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this
 // software and associated documentation files (the "Software"), to deal in the Software
 // without restriction, including without limitation the rights to use, copy, modify, merge,
 // publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
 // to whom the Software is furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all copies or
 // substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
 // INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
 // PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
@@ -38,7 +38,7 @@ namespace ICSharpCode.Decompiler.TypeSystem.Implementation
 		public IType Modifier => modifier;
 		public override TypeKind Kind => kind;
 
-		public override string NameSuffix => (kind == TypeKind.ModReq ? "modreq" : "modopt") + $"({modifier.FullName})";
+		public override string NameSuffix => (kind == TypeKind.ModReq ? " modreq" : " modopt") + $"({modifier.FullName})";
 
 		public override bool? IsReferenceType => elementType.IsReferenceType;
 		public override bool IsByRefLike => elementType.IsByRefLike;
@@ -97,7 +97,7 @@ namespace ICSharpCode.Decompiler.TypeSystem.Implementation
 		{
 			return elementType.GetProperties(filter, options);
 		}
-		
+
 		public override IType VisitChildren(TypeVisitor visitor)
 		{
 			var newElementType = elementType.AcceptVisitor(visitor);
