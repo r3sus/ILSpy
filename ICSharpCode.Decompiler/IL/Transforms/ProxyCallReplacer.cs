c09e54aaaba1162735307c8f5c5d74ff174f8b33
ElektroKill <skullmaster0103@gmail.com>
2021-07-15 20:39:55 +0200

    equal to 5.0-preview1

--- ICSharpCode.Decompiler/IL/Transforms/ProxyCallReplacer.cs
+++ ICSharpCode.Decompiler/IL/Transforms/ProxyCallReplacer.cs
@@ -40,25 +40,31 @@ namespace ICSharpCode.Decompiler.IL.Transforms
 			if (blockContainer.Blocks.Count != 1)
 				return;
 			var block = blockContainer.Blocks[0];
-			Call call = null;
-			if (block.Instructions.Count == 1) {
-				// leave IL_0000 (call Test(ldloc this, ldloc A_1))
-				if (!block.Instructions[0].MatchLeave(blockContainer, out ILInstruction returnValue))
-					return;
-				call = returnValue as Call;
-			} else if (block.Instructions.Count == 2) {
-				// call Test(ldloc this, ldloc A_1)
-				// leave IL_0000(nop)
-				call = block.Instructions[0] as Call;
-				if (!block.Instructions[1].MatchLeave(blockContainer, out ILInstruction returnValue))
-					return;
-				if (!returnValue.MatchNop())
+			Call call;
+			ILInstruction returnValue;
+			switch (block.Instructions.Count) {
+				case 1:
+					// leave IL_0000 (call Test(ldloc this, ldloc A_1))
+					if (!block.Instructions[0].MatchLeave(blockContainer, out returnValue))
+						return;
+					call = returnValue as Call;
+					break;
+				case 2:
+					// call Test(ldloc this, ldloc A_1)
+					// leave IL_0000(nop)
+					call = block.Instructions[0] as Call;
+					if (!block.Instructions[1].MatchLeave(blockContainer, out returnValue))
+						return;
+					if (!returnValue.MatchNop())
+						return;
+					break;
+				default:
 					return;
 			}
 			if (call == null || call.Method.IsConstructor) {
 				return;
 			}
-			if (call.Method.IsStatic != inst.Method.IsStatic || call.Method.Parameters.Count != inst.Method.Parameters.Count) {
+			if (call.Method.IsStatic || call.Method.Parameters.Count != inst.Method.Parameters.Count) {
 				return;
 			}
 			// check if original arguments are only correct ldloc calls
