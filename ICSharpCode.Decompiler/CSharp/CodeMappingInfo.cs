5515c29556f18e7d106bb0bdfdcea3e6e2042a80
ElektroKill <skullmaster0103@gmail.com>
2021-07-16 12:55:51 +0200

    Add documentation for CodeMappingInfo.

--- ICSharpCode.Decompiler/CSharp/CodeMappingInfo.cs
+++ ICSharpCode.Decompiler/CSharp/CodeMappingInfo.cs
@@ -1,17 +1,51 @@
+// Copyright (c) 2018 Siegfried Pammer
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+// to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
 using System.Collections.Generic;
 using dnlib.DotNet;
 using ICSharpCode.Decompiler.TypeSystem;
 
 namespace ICSharpCode.Decompiler.CSharp
 {
+	/// <summary>
+	/// Describes which parts of the (compiler-generated) code belong to which user code.
+	/// A part could be:
+	/// - the body (method) of a lambda.
+	/// - the MoveNext method of async/yield state machines.
+	/// </summary>
 	public class CodeMappingInfo
 	{
+		/// <summary>
+		/// The module containing the code.
+		/// </summary>
 		public PEFile Module { get; }
+
+		/// <summary>
+		/// The (parent) TypeDef containing the code.
+		/// </summary>
 		public TypeDef TypeDefinition { get; }
 
-		Dictionary<MethodDef, List<MethodDef>> parts;
-		Dictionary<MethodDef, MethodDef> parents;
+		readonly Dictionary<MethodDef, List<MethodDef>> parts;
+		readonly Dictionary<MethodDef, MethodDef> parents;
 
+		/// <summary>
+		/// Creates a <see cref="CodeMappingInfo"/> instance using the given <paramref name="module"/> and <paramref name="type"/>.
+		/// </summary>
 		public CodeMappingInfo(PEFile module, TypeDef type)
 		{
 			this.Module = module;
@@ -20,6 +54,11 @@ namespace ICSharpCode.Decompiler.CSharp
 			this.parents = new Dictionary<MethodDef, MethodDef>();
 		}
 
+		/// <summary>
+		/// Returns all parts of a method.
+		/// A method has at least one part, that is, the method itself.
+		/// If no parts are found, only the method itself is returned.
+		/// </summary>
 		public IEnumerable<MethodDef> GetMethodParts(MethodDef method)
 		{
 			if (parts.TryGetValue(method, out var p))
@@ -27,6 +66,12 @@ namespace ICSharpCode.Decompiler.CSharp
 			return new[] { method };
 		}
 
+		/// <summary>
+		/// Returns the parent of a part.
+		/// The parent is usually the "calling method" of lambdas, async and yield state machines.
+		/// The "calling method" has itself as parent.
+		/// If no parent is found, the method itself is returned.
+		/// </summary>
 		public MethodDef GetParentMethod(MethodDef method)
 		{
 			if (parents.TryGetValue(method, out var p))
@@ -34,6 +79,9 @@ namespace ICSharpCode.Decompiler.CSharp
 			return method;
 		}
 
+		/// <summary>
+		/// Adds a bidirectional mapping between <paramref name="parent"/> and <paramref name="part"/>.
+		/// </summary>
 		public void AddMapping(MethodDef parent, MethodDef part)
 		{
 			//Debug.Print("Parent: " + MetadataTokens.GetRowNumber(parent) + " Part: " + MetadataTokens.GetRowNumber(part));
