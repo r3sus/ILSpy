af930e82818ac526dcdcb69b7e122e358545b76d
ElektroKill <skullmaster0103@gmail.com>
2021-07-27 20:13:45 +0200

    Merge pull request #1561 from icsharpcode/mcs
    Improved support for closures generated by Mono C# compiler

--- ICSharpCode.Decompiler/CSharp/CSharpDecompiler.cs
+++ ICSharpCode.Decompiler/CSharp/CSharpDecompiler.cs
@@ -148,6 +148,7 @@ namespace ICSharpCode.Decompiler.CSharp
 				},
 				new ProxyCallReplacer(),
 				new DelegateConstruction(),
+				new TransformDisplayClassUsage(),
 				new HighLevelLoopTransform(),
 				new ReduceNestingTransform(),
 				new IntroduceDynamicTypeOnLocals(),
