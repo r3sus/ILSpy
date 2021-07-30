winnerdev2012/unity2023-ChatGPT

Fix #1709: NullPropagationTransform.IsGetter on generic types

ICSharpCode.Decompiler/CSharp/StatementBuilder.cs
@@ -791,7 +791,7 @@ namespace ICSharpCode.Decompiler.CSharp
 		bool ParentIsCurrentGetter(ILInstruction inst)
 		{
 			return inst.Parent is CallInstruction cv && cv.Method.IsAccessor &&
-				cv.Method.AccessorOwner is IProperty p && p.Getter.Equals(cv.Method);
+				cv.Method.AccessorKind == System.Reflection.MethodSemanticsAttributes.Getter;
 		}
 		#endregion
 

ICSharpCode.Decompiler/IL/Transforms/NullPropagationTransform.cs
@@ -272,7 +272,7 @@ namespace ICSharpCode.Decompiler.IL.Transforms
 
 		static bool IsGetter(IMethod method)
 		{
-			return method.AccessorOwner is IProperty p && p.Getter == method;
+			return method.AccessorKind == System.Reflection.MethodSemanticsAttributes.Getter;
 		}
 
 		private void IntroduceUnwrap(ILVariable testedVar, ILInstruction varLoad, Mode mode)

r3sus/ILSpy

ICSharpCode.Decompiler/CSharp/StatementBuilder.cs
@@ -790,7 +790,7 @@ namespace ICSharpCode.Decompiler.CSharp
 		bool ParentIsCurrentGetter(ILInstruction inst)
 		{
 			return inst.Parent is CallInstruction cv && cv.Method.IsAccessor &&
-				cv.Method.AccessorOwner is IProperty p && p.Getter.Equals(cv.Method);
+				cv.Method.AccessorKind == dnlib.DotNet.MethodSemanticsAttributes.Getter;
 		}
 		#endregion
 

ICSharpCode.Decompiler/IL/Transforms/NullPropagationTransform.cs
@@ -272,7 +272,7 @@ namespace ICSharpCode.Decompiler.IL.Transforms
 
 		static bool IsGetter(IMethod method)
 		{
-			return method.AccessorOwner is IProperty p && p.Getter == method;
+			return method.AccessorKind == dnlib.DotNet.MethodSemanticsAttributes.Getter;
 		}
 
 		private void IntroduceUnwrap(ILVariable testedVar, ILInstruction varLoad, Mode mode)