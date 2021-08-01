b464f134189cdb99bce4fbcf3a00dd53804ca564
ElektroKill <skullmaster0103@gmail.com>
2021-08-01 12:03:58 +0200

    Set version to 5.0.2

--- ILSpy/Properties/AssemblyInfo.cs
+++ ILSpy/Properties/AssemblyInfo.cs
@@ -40,7 +40,7 @@ internal static class RevisionClass
 {
 	public const string Major = "5";
 	public const string Minor = "0";
-	public const string Build = "1";
+	public const string Build = "2";
 	public const string Revision = "0";
 	public const string VersionName = null;
 
