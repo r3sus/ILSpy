7697423589476562981404d891c07835df5b7a32
ElektroKill <skullmaster0103@gmail.com>
2021-08-02 11:12:37 +0200

    Set version to 6.0-preview1

--- ILSpy/MainWindow.xaml.cs
+++ ILSpy/MainWindow.xaml.cs
@@ -340,9 +340,7 @@ namespace ICSharpCode.ILSpy
 			}
 
 			Dispatcher.BeginInvoke(DispatcherPriority.Loaded, new Action(() => OpenAssemblies(spySettings)));
-#if DEBUG
 			this.Title = $"ILSpy {RevisionClass.FullVersion}";
-#endif
 		}
 
 		void OpenAssemblies(ILSpySettings spySettings)
