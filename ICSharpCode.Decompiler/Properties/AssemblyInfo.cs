e640b80d3c153108fbb1e5492bf6ffa71d89b7f6
ElektroKill <skullmaster0103@gmail.com>
2021-08-08 19:56:05 +0200

    Set version to 6.2-preview1

--- ICSharpCode.Decompiler/Properties/AssemblyInfo.cs
+++ ICSharpCode.Decompiler/Properties/AssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Diagnostics.CodeAnalysis;
 // If you need to expose a type to COM, use [ComVisible(true)] on that type.
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("6.1.0.0")]
-[assembly: AssemblyInformationalVersion("6.1.0.0")]
+[assembly: AssemblyVersion("6.2.0.0")]
+[assembly: AssemblyInformationalVersion("6.2.0.0-preview1")]
 
 [assembly: InternalsVisibleTo("ICSharpCode.Decompiler.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001004dcf3979c4e902efa4dd2163a039701ed5822e6f1134d77737296abbb97bf0803083cfb2117b4f5446a217782f5c7c634f9fe1fc60b4c11d62c5b3d33545036706296d31903ddcf750875db38a8ac379512f51620bb948c94d0831125fbc5fe63707cbb93f48c1459c4d1749eb7ac5e681a2f0d6d7c60fa527a3c0b8f92b02bf")]
 
