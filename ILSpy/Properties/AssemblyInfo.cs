025c712fb7dac71a12cf84bf99903f2b17ac703a
ElektroKill <skullmaster0103@gmail.com>
2021-08-11 13:30:47 +0200

    Set version to 7.0

--- ILSpy/Properties/AssemblyInfo.cs
+++ ILSpy/Properties/AssemblyInfo.cs
@@ -42,7 +42,7 @@ internal static class RevisionClass
 	public const string Minor = "0";
 	public const string Build = "0";
 	public const string Revision = "0";
-	public const string VersionName = "preview3";
+	public const string VersionName = null;
 
 	public const string FullVersion = Major + "." + Minor + "." + Build + "." + Revision + (VersionName != null ? "-" + VersionName : "");
 }
