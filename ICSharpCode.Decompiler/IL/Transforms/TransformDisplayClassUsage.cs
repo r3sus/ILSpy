b9a2c0851ee8a66173db1faaedd8faf8041c46b7
ElektroKill <skullmaster0103@gmail.com>
2021-08-11 19:19:33 +0200

    Fix #2405: Assertion fail in TDCU

--- ICSharpCode.Decompiler/IL/Transforms/TransformDisplayClassUsage.cs
+++ ICSharpCode.Decompiler/IL/Transforms/TransformDisplayClassUsage.cs
@@ -586,9 +586,17 @@ namespace ICSharpCode.Decompiler.IL.Transforms
 			if (!function.Method.IsStatic && FindThisField(out var thisField)) {
 				var thisVar = function.Variables
 					.FirstOrDefault(t => t.IsThis() && t.Type.GetDefinition() == decompilationContext.CurrentTypeDefinition);
-				if (thisVar == null) {
-					thisVar = new ILVariable(VariableKind.Parameter, decompilationContext.CurrentTypeDefinition, -1) { Name = "this" };
+				if (thisVar == null)
+				{
+					thisVar = new ILVariable(VariableKind.Parameter, decompilationContext.CurrentTypeDefinition, -1) {
+						Name = "this", StateMachineField = thisField
+					};
 					function.Variables.Add(thisVar);
+				}
+				else if (thisVar.StateMachineField != null && displayClass.VariablesToDeclare.ContainsKey(thisVar.StateMachineField))
+				{
+					// "this" was already added previously, no need to add it twice.
+					return displayClass;
 				}
 				VariableToDeclare variableToDeclare = new VariableToDeclare(displayClass, thisField, thisVar);
 				displayClass.VariablesToDeclare.Add(thisField, variableToDeclare);
